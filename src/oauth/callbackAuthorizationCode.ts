29448042411abdd9a53aa621b19b16c5d57b44b9
Thomas Caillier <thomascaillier@gmail.com>
2021-05-29 11:47:04 +0200

    Try fix Authorization header

--- src/oauth/callbackAuthorizationCode.ts
+++ src/oauth/callbackAuthorizationCode.ts
@@ -1,20 +1,16 @@
 import axios from "axios";
 
 export async function callbackAuthorizationCode(req: any, res: any) {
-	//res.send("Callback code is " + req.query.code);
 	axios
 		.get("https://pixelfed.social/api/v1/accounts/verify_credentials", {
 			headers: {
-				Authorization: req.query.code,
+				Authorization: `Bearer ${req.query.code}`,
 			},
 		})
 		.then((response: any) => {
-            res.send(response);
-			console.log(`statusCode: ${response.statusCode}`);
-			console.log(response);
+            res.send(`<p>Code : ${req.query.code}</p><p>Response : </p><pre>${response}</pre>`);
 		})
 		.catch((error) => {
-            res.send(error);
-			console.error(error);
+            res.send(`<p>Code : ${req.query.code}</p><p>Error : </p><pre>${error}</pre>`);
 		});
 }
